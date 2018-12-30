f94b35d7418caf3967a1ec5bf7d6c9c9714bcbf0
EnimaTita <tabtani.m.amine@gmail.com>
2018-12-30 01:30:28 +0100

    add movie controller

---
+++ VignyWebApplication/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VignyWebApplication.Models;
+
+namespace VignyWebApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+			var movie = new Movie() { Name="Harry Potter" } ;
+            return View(movie);
+        }
+    }
+}
